57831712ec2bc2176027201954c7ea19e6c09333
Kizuna Yuuki <kizunayuuki@Kizunas-iMac.local>
2022-12-12 10:14:28 +0700

    65

--- index.js
+++ index.js
@@ -38,9 +38,13 @@ app.get('/contact', (req, res) => {
     //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
     res.render('contact');
 })
-app.get('/post', (req, res) => {
-    //res.sendFile(path.resolve(__dirname,'pages/post.html'))
-    res.render('post')
+
+app.get('/post/:id', async (req, res) => {
+    console.log(req.params)
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post', {
+        blogpost
+    })
 })
 
 app.get('/posts/new', (req, res) => {
